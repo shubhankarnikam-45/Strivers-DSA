dbdd47adf000c24fa705eb7ed6636f7c74f93f65
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2023-12-03 15:53:17 +0530

    detect cycle in directed graph using khan's algorithm

--- 15. Graphs/15.3 Topo Sort and Problems/02. Kahn's Algorithm (Topo Sort Using BFS)/kahnsAlgorithm.java
+++ 15. Graphs/15.3 Topo Sort and Problems/02. Kahn's Algorithm (Topo Sort Using BFS)/kahnsAlgorithm.java
@@ -1,6 +1,6 @@
 //que link
 //https://www.codingninjas.com/studio/problems/topological-sorting_973003?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
-
+//dd
 import java.util.*;
 public class Solution {
     public static class Edge
