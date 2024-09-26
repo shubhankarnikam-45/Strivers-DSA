1aec1d58c743b4f7d397169a41d22821ac5c2214
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2024-09-26 16:09:30 +0530

    min coin using memoization

---
+++ 16. Dynamic Programming(DP)/16.4 DP on Subsequence/09. Minimum Coin 2/usingMemoization.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class usingMemoization {
+
+	public static long fun(int index, int value, int deno[], long dp[][])
+	{
+
+		
+		//base case.
+		if(value == 0) return 1;
+
+		if(index == 0)
+		{
+			if(value % deno[index] == 0) return 1;
+			else return 0;
+		}
+
+		// if(dp[index][value] != -1) return 0;
+
+		long notPick = fun(index - 1, value, deno, dp);
+		
+		long pick = 0;
+		if(deno[index] <= value)
+		{
+			pick = fun(index, value - deno[index], deno, dp);
+		}
+
+		return  notPick + pick;
+	}
+	public static long countWaysToMakeChange(int denominations[], int value){
+        
+		//length of denomination.
+		int n = denominations.length;
+
+		long dp[][] = new long[n][value+1];
+		for(long arr[] : dp)   Arrays.fill(arr, -1); 
+
+		return fun(n-1, value, denominations, dp) ;
+
+	}
+	
+}
