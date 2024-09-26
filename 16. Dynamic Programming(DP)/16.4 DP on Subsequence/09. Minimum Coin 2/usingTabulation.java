9a0a4e96b213eeca71a6e5ed12d602cc84f20d4b
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2024-09-26 16:29:08 +0530

    min coin using tabulation

--- 16. Dynamic Programming(DP)/16.4 DP on Subsequence/09. Minimum Coin 2/usingTabulation.java
+++ 16. Dynamic Programming(DP)/16.4 DP on Subsequence/09. Minimum Coin 2/usingTabulation.java
@@ -1,3 +1,42 @@
+import java.util.*;
 public class usingTabulation {
-    
+
+	
+	public static long countWaysToMakeChange(int denominations[], int value){
+        
+		//length of denomination.
+		int n = denominations.length;
+
+		long dp[][] = new long[n][value+1];
+
+		//Tabulation.
+
+		//base case.
+		for(int target = 0; target <= value; target++)
+		{
+			if(target % denominations[0] == 0) dp[0][target] = 1;
+			else dp[0][target] = 0;
+			
+		}
+
+		for(int index=1; index < n; index++)
+		{
+			for(int target = 0; target<= value; target++)
+			{
+				long notPick = dp[index - 1][target];
+		
+				long pick = 0;
+				if(denominations[index] <= target)
+				{
+					pick = dp[index][target - denominations[index]];
+				}
+
+				dp[index][target] = notPick + pick;
+			}
+		}
+
+		return dp[n-1][value];
+
+	}
+	
 }
