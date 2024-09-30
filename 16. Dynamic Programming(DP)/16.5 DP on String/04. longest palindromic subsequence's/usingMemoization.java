492642085f111e237c240cbc3524225129be09c9
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2024-09-30 20:55:23 +0530

    using tabulation

---
+++ 16. Dynamic Programming(DP)/16.5 DP on String/04. longest palindromic subsequence's/usingMemoization.java
@@ -0,0 +1,31 @@
+
+//question link.
+// https://leetcode.com/problems/longest-palindromic-subsequence/
+
+class usingMemoization {
+    public int fun(int left, int right, String s, int dp[][])
+    {
+        //base case.
+        if(left == right) return 1;
+        if(left > right) return 0;
+
+        if(dp[left][right] != -1) return dp[left][right];
+
+        if(s.charAt(left) == s.charAt(right))
+        {
+            return dp[left][right] = 2 + fun(left + 1, right - 1, s, dp);
+        }
+        
+        return dp[left][right] = Math.max(fun(left + 1, right, s, dp), fun(left, right - 1, s, dp));
+
+
+    }
+    public int longestPalindromeSubseq(String s) {
+        
+        int m = s.length();
+        int dp[][] = new int[m][m];
+        for(int arr[] : dp) Arrays.fill(arr, -1);
+
+        return fun(0, m - 1, s, dp); 
+    }
+}
