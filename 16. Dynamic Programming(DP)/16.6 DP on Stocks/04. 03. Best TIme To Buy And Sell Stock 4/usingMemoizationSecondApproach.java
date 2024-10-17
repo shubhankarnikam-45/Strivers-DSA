c8065b2b398f784b95c281c2822cfe4be1759bec
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2024-10-17 15:58:11 +0530

    using memoization second approach differnent from previous question

--- 16. Dynamic Programming(DP)/16.6 DP on Stocks/04. 03. Best TIme To Buy And Sell Stock 4/usingMemoizationSecondApproach.java
+++ 16. Dynamic Programming(DP)/16.6 DP on Stocks/04. 03. Best TIme To Buy And Sell Stock 4/usingMemoizationSecondApproach.java
@@ -2,26 +2,34 @@
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
 
 class usingMemoizationSecondApproach {
-    public int fun(int ind, int transNo, int k, int prices[])
+    public int fun(int ind, int transNo, int k, int prices[], int dp[][])
     {
         //base case.
         if(ind == prices.length || transNo == k * 2) return 0;
+
+
+        //if answer already exists
+        if(dp[ind][transNo] != -1) return dp[ind][transNo];
 
         int profit = 0;
 
         if(transNo % 2 == 0) //even ==> Buy
         {
-            profit = Math.max(-prices[ind] + fun(ind + 1, transNo + 1, k,  prices), 0 + fun(ind + 1, transNo, k, prices));
+            profit = Math.max(-prices[ind] + fun(ind + 1, transNo + 1, k,  prices, dp), 0 + fun(ind + 1, transNo, k, prices, dp));
         }
         else //odd ==> Sell
         {
-            profit = Math.max(prices[ind] + fun(ind + 1, transNo + 1, k , prices), 0 + fun(ind + 1, transNo, k, prices));
+            profit = Math.max(prices[ind] + fun(ind + 1, transNo + 1, k , prices, dp), 0 + fun(ind + 1, transNo, k, prices, dp));
         }
 
-        return profit;
+        return dp[ind][transNo] = profit;
     }
     public int maxProfit(int k, int[] prices) {
         
-        return fun(0, 0, k, prices);
+        int n = prices.length;
+        int dp[][] = new int[n][k * 2];
+
+        for(int arr[] : dp) Arrays.fill(arr, -1);
+        return fun(0, 0, k, prices, dp);
     }
 }
