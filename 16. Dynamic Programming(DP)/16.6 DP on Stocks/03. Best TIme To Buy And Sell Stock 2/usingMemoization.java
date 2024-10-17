cec933901306e6a86f0067e515553849bebff7fa
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2024-10-17 10:37:27 +0530

    solve problem using memoization

--- 16. Dynamic Programming(DP)/16.6 DP on Stocks/03. Best TIme To Buy And Sell Stock 2/usingMemoization.java
+++ 16. Dynamic Programming(DP)/16.6 DP on Stocks/03. Best TIme To Buy And Sell Stock 2/usingMemoization.java
@@ -2,29 +2,45 @@
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/submissions/1424996301/
 
 class usingMemoization {
-    public int fun(int ind, int buy, int cnt, int prices[])
-    {
-        //base case.
-        if(ind == prices.length) return 0;
-        if(cnt == 2) return 0;
+    public int fun(int ind, int buy, int cnt, int prices[], int dp[][][]) {
+        // base case.
+        if (ind == prices.length)
+            return 0;
+        if (cnt == 2)
+            return 0;
+
+        // if answe already exists.
+        if (dp[ind][buy][cnt] != -1)
+            return dp[ind][buy][cnt];
 
         int profit = 0;
 
-        if(buy == 1) //buy
+        if (buy == 1) // buy
         {
-            profit = Math.max(-prices[ind] + fun(ind + 1, 0, cnt , prices), 0 + fun(ind + 1, 1, cnt, prices));   
-        }
-        else //sell
+            profit = Math.max(-prices[ind] + fun(ind + 1, 0, cnt, prices, dp), 0 + fun(ind + 1, 1, cnt, prices, dp));
+        } else // sell
         {
-            profit = Math.max(prices[ind] + fun(ind + 1, 1, cnt + 1, prices), 0 + fun(ind + 1, 0, cnt, prices));
+            profit = Math.max(prices[ind] + fun(ind + 1, 1, cnt + 1, prices, dp), 0 + fun(ind + 1, 0, cnt, prices, dp));
         }
 
-        return profit;
+        return dp[ind][buy][cnt] = profit;
     }
+
     public int maxProfit(int[] prices) {
-        
+
         int n = prices.length;
 
-        return fun(0, 1, 0, prices);
+        // creating the 3d dp to optimize this.
+        int dp[][][] = new int[n][2][2];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    dp[i][j][k] = -1;
+                }
+            }
+        }
+
+        return fun(0, 1, 0, prices, dp);
     }
 }
---
+++ 16. Dynamic Programming(DP)/16.6 DP on Stocks/03. Best TIme To Buy And Sell Stock 2/usingTabulation.java
@@ -0,0 +1,2 @@
+usingTabulation
+
